7c2fd37d943b1fb568133e8c932fb35fc6e0d259
Purplesamosa <julian_carlson@live.com>
2022-05-22 22:07:43 +0100

    added progress track to runner
    Needs a call on distance increase to set new position

---
+++ Assets/Scripts/UI/FiniteRunner/UIRunnerManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class UIRunnerManager : MonoBehaviour
+{
+    public RectTransform m_Track;
+    public RectTransform m_MovingPointer;
+
+    public float X_MinValue = -820.0f; //used to offset the goal pointer;
+    public float X_MaxValue = 820.0f;
+
+    public float m_MaxDistance;
+
+    //TEST CODE 
+    /*public float distance = 0;
+
+    private void Start()
+    {
+        m_MaxDistance = 10000;
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            distance += 130;
+            SetPlayerProgress(distance);
+        }
+    }*/
+
+    public void SetMaxDistance(float maxDistance)
+    {
+        m_MaxDistance = maxDistance;
+    }
+
+    public void SetPlayerProgress(float distance)
+    {
+        float percentProgress = distance / m_MaxDistance;
+
+        float uiPosition = Mathf.Lerp(X_MinValue,X_MaxValue,percentProgress);
+
+        m_MovingPointer.localPosition = new Vector3 (uiPosition,m_MovingPointer.localPosition.y,0);
+
+    }
+}
